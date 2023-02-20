343159d14f48cc10eea8d03efd04d4b78f970f53
MegaISari <misari@cpp.edu>
2023-02-20 00:47:20 -0800

    first commit

---
+++ hello/src/main/java/com/meepsun/hello/Hello.java
@@ -0,0 +1,14 @@
+package com.meepsun.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
